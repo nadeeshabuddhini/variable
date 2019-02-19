f768e8b909a0f7d6b576dea79d2887e371b80ff4
Nadeesha Buddhini <hknadeesha1995buddhi@gmail.com>
2019-02-19 23:27:36 +0530

    variable
    variable

---
+++ src/variable/Variable.java
@@ -0,0 +1,16 @@
+
+package variable;
+
+public class Variable {
+
+    public static void main(String[] args) {
+     int num1=30;
+     int num2=50;
+     
+     int sum=num1+num2;
+     
+     System.out.println("Sum is "+sum);
+     
+    }
+    
+}
